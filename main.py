85ed5b62c716d904ecdb139eff6a56f7de9e5703
zjedens <z.j.edens@gmail.com>
2026-06-16 17:44:33 -0500

    Added ability to send user-provided prompts to Gemini and receive responses. Added verbosity option to print original prompt as well as token usage for prompt and response.

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-def main():
-    print("Hello from agent-smith!")
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
+
+def main():
+    parser = argparse.ArgumentParser(description="AI ChatBot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError("Gemini API key not found")
+
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages
+    )
+
+    #Print prompt and token usage if args.verbose == True
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    # Print response
+    print(response.text)
 
 if __name__ == "__main__":
     main()
